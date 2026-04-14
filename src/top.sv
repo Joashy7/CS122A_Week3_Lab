0934ea7152da526f9ad7e8c2dff953e8a9b8ae89
Glalie <jemjemmarcos@gmail.com>
2026-04-13 17:36:25 -0700

    Adding Exercise 1 functionality

--- src/top.sv
+++ src/top.sv
@@ -1,8 +1,29 @@
 module top (
-    /** Input Ports */
-    /** Output Ports */
+    input logic clk,
+    input wire button1,
+    input wire button2,
+    output logic out
 );
+    typedef enum logic [3:0]{START, S0, S1, S2, S3} state_t;
 
-/** Logic */
+    state_t state = START;
+
+    always @(posedge clk) begin
+        case (state)
+            START:state = S0;
+            S0:state = state_t'(but1 ? S1 : S0);
+            S1:state = state_t'(!but1 ? S1 : S2);
+            S2:state = state_t'(but1 ? S3 : S2);
+            S3:state = state_t'(!but1 ? S3 : S0);
+            default: state = START;
+        endcase
+    end
+
+    always_comb begin
+        case(state)
+            S0: out = 0;
+            S2: out = 1;
+        endcase 
+    end
 
 endmodule
--- tb/top_tb.sv
+++ tb/top_tb.sv
@@ -15,15 +15,19 @@ module top_tb;
 
 /** declare tb signals below */
 logic clk_tb;
+wire but1;
+wire but2;
+logic out;
 
 /** declare module(s) below */
-top dut                    // declare an inst of top called "dut" (device under test)
-(
+top dut (                  // declare an inst of top called "dut" (device under test)
     /** hook up tb signals to dut signals */
     .clk(clk_tb)           // connect dut's clk wire to clk_tb
+    .button1(but1)
+    .button2(but2)
 );
 
-localparam CLK_PERIOD = /** clk period */;
+localparam CLK_PERIOD = 2 /** clk period */;
 always #(CLK_PERIOD/2) clk_tb=~clk_tb;          // toggle clk_tb every #(CLK_PERIOD/2) ticks
 
 initial begin
@@ -31,10 +35,46 @@ initial begin
     $dumpvars(0, top_tb);       // capture all signals under top_tb
 end
 
+task checkLED(); begin
+if (out) begin
+        $display("LED State: On");
+    end else begin
+        $display ("LED State: Off");
+    end
+end
+endtask
+
+task testPress(); begin
+    clk_tb<=1'b1;       // sets clk_tb to 1
+    but1 = 1;
+    checkLED();
+    #5; 
+    but1 = 0;
+    checkLED();
+end
+endtask
+
+task testPressTwice(); begin
+    clk_tb<=1'b1;       // sets clk_tb to 1
+    but1 = 1;
+    checkLED();
+    #5; 
+    but1 = 0;
+    checkLED();
+    #5;
+    but1 = 1;
+    checkLED();
+    #5; 
+    but1 = 0;
+    checkLED();
+end
+endtask
+
 initial begin
     /** testbench logic goes below */
-    clk_tb<=1'b1;       // sets clk_tb to 1
-    #(CLK_PERIOD*3);    // waits for CLK_PERIOD * 3 ticks
+    testPress();     
+    testPressTwice();
+    //#(CLK_PERIOD*100);    // waits for CLK_PERIOD * 100 ticks
     $finish;            // end simulation, otherwise it runs indefinitely
 end
 
