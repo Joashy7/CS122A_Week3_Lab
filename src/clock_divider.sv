31f2b43d48cd7a9991f80bc0f4e9c44c35fd362c
HMasroor3 <hoomanmasrour@gmail.com>
2026-04-15 15:47:17 -0700

    exercise 2 done, made the clock divider

--- src/clock_divider.sv
+++ src/clock_divider.sv
@@ -5,3 +5,17 @@ module clock_divider (
     output wire led
 );
 
+logic [63:0] clk_div_counter;
+wire clk_0s25;
+
+assign clk_0s25 = clk_div_counter[22]; // assuming 25MHz toggle roughly every 0.25s
+
+always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+        clk_div_counter <= 0;
+    end else begin
+        clk_div_counter <= clk_div_counter + 1;
+    end
+end
+
+endmodule
