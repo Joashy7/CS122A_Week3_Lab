b60bade1ee46160d74d775e6e411be32026e9648
HMasroor3 <hoomanmasrour@gmail.com>
2026-04-15 15:46:10 -0700

    removed unnecessary button2 input and changed parameter types

--- src/button.sv
+++ src/button.sv
@@ -1,8 +1,7 @@
 module button (
-    input logic clk,
-    input reg button1,
-    input reg button2,
-    output reg out
+    input wire clk,
+    input wire button,
+    output logic out
 );
     typedef enum logic [3:0]{START, S0, S1, S2, S3} state_t;
 
@@ -11,10 +10,10 @@ module button (
     always @(posedge clk) begin
         case (state)
             START:state = S0;
-            S0:state = state_t'(button1 ? S1 : S0);
-            S1:state = state_t'(!button1 ? S2 : S1);
-            S2:state = state_t'(button1 ? S3 : S2);
-            S3:state = state_t'(!button1 ? S0 : S3);
+            S0:state = state_t'(button ? S1 : S0);
+            S1:state = state_t'(!button ? S2 : S1);
+            S2:state = state_t'(button ? S3 : S2);
+            S3:state = state_t'(!button ? S0 : S3);
             default: state = START;
         endcase
     end
