8728344a73941f6d304e762ef5f7b263ca2eedee
Joashy <91395897+Joashy7@users.noreply.github.com>
2026-04-20 21:07:38 -0700

    Invert switch assignment for dp signal

--- src/top.sv
+++ src/top.sv
@@ -28,6 +28,6 @@ pwm pwm_red (.clk(clk), .duty_cycle(duty_cycle_red), .out(led_red));
 switch display_switch (.switch(switch), .duty_cycle_blue(duty_cycle_blue), .duty_cycle_red(duty_cycle_red), .display_duty_cycle(displayed_duty_cycle));
 decoder decoder (.clk(clk), .bcd(displayed_duty_cycle), .seg7(seg7));
 
-assign dp = switch;
+assign dp = !switch;
 
 endmodule
